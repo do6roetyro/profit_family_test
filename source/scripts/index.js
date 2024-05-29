06e7ed0433495f81a915f3671ec24bf69cf8814e
dobroeytro <warm.citrus@gmail.com>
2024-05-30 00:34:20 +0300

    feat: task1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,17 +1,27 @@
-// -- Меню навигации на мобилке.
-const menu = document.querySelector('.main-nav__list');
-const buttonMenu = document.querySelector('.main-nav__button-menu');
-const logo = document.querySelector('.main-nav__logo-link');
-const map = document.querySelector('.location__map');
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-buttonMenu.onclick = function () {
-  menu.classList.toggle('main-nav__list--close');
-  buttonMenu.classList.toggle('main-nav__button-menu--cross');
-  logo.classList.toggle('main-nav__logo-link--close');
-};
 
-// -- nojs реализация.
-menu.classList.remove('main-nav__list--noscript');
-map?.classList.remove('location__map--nojs');
-buttonMenu.classList.remove('main-nav__button-menu--noscript');
-// logo.classList.add("main-nav__logo--closed-menu");
+const swiper = new Swiper('.swiper', {
+
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  loop: true,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
